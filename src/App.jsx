adc70b3e3417fc414684451ddc7e9f9edb528f4f
Juan Alejandro Moreno Santos <128493014+JuanMoreno1902@users.noreply.github.com>
2026-07-06 18:36:21 -0500

    Fix import path for projects data

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { projects } from "./data/projects";
+import { projects } from "./Data/projects";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero.jsx";
